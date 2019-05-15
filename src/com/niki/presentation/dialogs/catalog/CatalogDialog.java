d7b47d51e6837d370d003ca30de59bf58f5ceca6
nikita <justlordmlg@gmail.com>
2019-05-15 20:00:32 +0300

    Added login dialog

---
+++ src/com/niki/presentation/dialogs/catalog/CatalogDialog.java
@@ -0,0 +1,40 @@
+package com.niki.presentation.dialogs.catalog;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class CatalogDialog extends JDialog {
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JButton buttonAdd;
+    private JButton deleteButton;
+    private JTable table1;
+
+    public CatalogDialog() {
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
+
+        buttonOK.addActionListener(e -> onOK());
+        buttonCancel.addActionListener(e -> onCancel());
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+        contentPane.registerKeyboardAction(e -> onCancel(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+    }
+
+    private void onOK() {
+        // buttonAdd your code here
+        dispose();
+    }
+
+    private void onCancel() {
+        // buttonAdd your code here if necessary
+        dispose();
+    }
+}
