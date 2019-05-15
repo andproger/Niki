18d179bb296f46fbf9e75d147adc396374a6841b
Andrey_Griboedov <Andrey_Griboedov@STYLESOFT.BY>
2019-05-15 19:09:06 +0300

    add sql connection

--- src/com/niki/presentation/app/Main.java
+++ src/com/niki/presentation/app/Main.java
@@ -1,12 +1,16 @@
 package com.niki.presentation.app;
 
+import com.niki.data.cache.Sql;
 import com.niki.presentation.dialogs.main.MainDialog;
 
 public class Main {
     public static void main(String[] args) {
+        Sql.init("25.48.169.93", "drugs", "admin", "admin");
+
         MainDialog dialog = new MainDialog();
         dialog.pack();
         dialog.setVisible(true);
+
         System.exit(0);
     }
 }
