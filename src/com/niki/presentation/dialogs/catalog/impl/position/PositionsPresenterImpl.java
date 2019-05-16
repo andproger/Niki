18009a94e84bbc06365de006819ed138a23ae14c
Nikita <justlordmlg@gmail.com>
2019-05-16 16:32:59 +0300

    Added position repository

--- src/com/niki/presentation/dialogs/catalog/impl/position/PositionsPresenterImpl.java
+++ src/com/niki/presentation/dialogs/catalog/impl/position/PositionsPresenterImpl.java
@@ -1,11 +1,47 @@
 package com.niki.presentation.dialogs.catalog.impl.position;
 
+import com.niki.domain.entities.Position;
+import com.niki.domain.gateways.repositories.PositionRepository;
 import com.niki.presentation.dialogs.catalog.BaseCatalogPresenter;
 import com.niki.presentation.dialogs.catalog.CatalogView;
 
-public class PositionsPresenterImpl extends BaseCatalogPresenter {
+import java.util.ArrayList;
 
-    public PositionsPresenterImpl(CatalogView view) {
+public class PositionsPresenterImpl extends BaseCatalogPresenter {
+    private final PositionRepository positionRepository;
+
+    private PositionsTableModel tableModel;
+    private ArrayList<Position> positions;
+
+    public PositionsPresenterImpl(CatalogView view, PositionRepository positionRepository) {
         super(view);
+        this.positionRepository = positionRepository;
+
+        initTableModel();
+    }
+
+    private void initTableModel() {
+        this.positions = positionRepository.getPositions();
+        this.tableModel = new PositionsTableModel(positions);
+        view.setTableModel(tableModel);
+    }
+
+    @Override
+    public void onSaveClicked() {
+        positionRepository.savePositions(positions);
+    }
+
+    @Override
+    public void onAddClicked() {
+        positions.add(new Position(0, 0, ""));
+        tableModel.fireTableDataChanged();
+    }
+
+    @Override
+    public void onDeleteClicked(int[] rows) {
+        for (int i = rows.length - 1; i >= 0; i--)
+            this.positions.remove(rows[i]);
+
+        tableModel.fireTableDataChanged();
     }
 }
