13accc3925b9764e3fd2657ebb8b83710a0b18de
nikita <justlordmlg@gmail.com>
2019-05-22 10:59:04 +0300

    sale saving bug fixed

--- src/com/niki/data/cache/database/datastores/SqlSaleItemDataStore.java
+++ src/com/niki/data/cache/database/datastores/SqlSaleItemDataStore.java
@@ -3,6 +3,9 @@ package com.niki.data.cache.database.datastores;
 import com.niki.data.cache.database.datastores.base.SqlDataStore;
 import com.niki.domain.entities.SaleItem;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class SqlSaleItemDataStore extends SqlDataStore<SaleItem> implements SaleItemDataStore {
 
     public SqlSaleItemDataStore() {
@@ -12,5 +15,22 @@ public class SqlSaleItemDataStore extends SqlDataStore<SaleItem> implements Sale
     @Override
     protected SaleItem newItemInstance() {
         return new SaleItem();
+    }
+
+    @Override
+    public void save(ArrayList<SaleItem> items) {
+        try {
+            connection.prepareStatement(sqlGen.delete(null)).execute();
+            var statement = connection.prepareStatement(sqlGen.insert());
+
+            for (var item : items) {
+                prepareInsert(statement, item);
+                statement.execute();
+            }
+
+            statement.execute();
+        } catch (SQLException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
