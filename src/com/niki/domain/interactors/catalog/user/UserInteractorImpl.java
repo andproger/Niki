94238035853ac49f07feac5faf82e84103ad260c
Nikita <justlordmlg@gmail.com>
2019-05-30 09:20:43 +0300

    fixed user creation

--- src/com/niki/domain/interactors/catalog/user/UserInteractorImpl.java
+++ src/com/niki/domain/interactors/catalog/user/UserInteractorImpl.java
@@ -1,7 +1,9 @@
 package com.niki.domain.interactors.catalog.user;
 
 import com.niki.domain.entities.Position;
-import com.niki.domain.gateways.repositories.*;
+import com.niki.domain.entities.User;
+import com.niki.domain.gateways.repositories.PositionRepository;
+import com.niki.domain.gateways.repositories.UserRepository;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -23,8 +25,8 @@ public class UserInteractorImpl implements UserInteractor {
         var positions = positionRepository.get();
 
         var userContracts = new ArrayList<UserContract>();
-        for(var user : users){
-            var position = new Position(user.getPositionId(),0, "", false);
+        for (var user : users) {
+            var position = new Position(user.getPositionId(), 0, "", false);
             var index = Collections.binarySearch(positions, position, Comparator.comparingInt(Position::getId));
             position = index >= 0 ? positions.get(index) : null;
             userContracts.add(new UserContract(user.getId(), user.getLogin(), user.getPassword(), user.getFirstName(), user.getLastName(), position));
@@ -35,11 +37,31 @@ public class UserInteractorImpl implements UserInteractor {
 
     @Override
     public void saveUsers(List<UserContract> userContracts) {
-
+        userRepository.save(getUsersByContracts(userContracts));
     }
 
     @Override
     public List<Position> getPositions() {
         return positionRepository.get();
+    }
+
+
+    private List<User> getUsersByContracts(List<UserContract> contracts) {
+        var users = new ArrayList<User>();
+        for (var contract : contracts) {
+            users.add(getUserByContract(contract));
+        }
+
+        return users;
+    }
+
+    private User getUserByContract(UserContract contract) {
+        return new User(contract.getId(),
+                contract.getPosition().getId(),
+                contract.getLogin(),
+                contract.getPassword(),
+                contract.getFirstName(),
+                contract.getLastName()
+        );
     }
 }
